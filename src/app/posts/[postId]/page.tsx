42df1ec8d8f3cef0139e07cac869ce4ebbd1f25c
Roger <worth22focus3000@gmail.com>
2024-11-12 15:39:10 +0800

    feat: add post page and set dynamic meta data

---
+++ src/app/posts/[postId]/page.tsx
@@ -0,0 +1,66 @@
+import ClapButton from "@/components/ClapButton";
+import { delay } from "@/lib/utils";
+import { BlogPost, BlogPostsResponse } from "@/models/BlogPost";
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
+// import { cache } from "react";
+
+
+interface BlogPostPageProps   {
+  params: Promise<{ postId: string | number }>
+}
+
+export async function generateStaticParams() {
+  const response = await fetch("https://dummyjson.com/posts");
+  const { posts }: BlogPostsResponse = await response.json();
+
+  return posts.map((id) => ({ postId: id.toString() }));
+}
+
+// Manually deduplicate requests if not using fetch
+// const getPost = cache(async (postId: string) => {
+//   const post = await prisma.post.findUnique(postId);
+//   return post;
+// })
+
+export async function generateMetadata({
+  params,
+}: BlogPostPageProps): Promise<Metadata> {
+  const { postId } = await params
+  const response = await fetch(`https://dummyjson.com/posts/${postId}`);
+  const post: BlogPost = await response.json();
+
+  return {
+    title: post.title,
+    description: post.body,
+    // openGraph: {
+    //   images: [
+    //     {
+    //       url: post.imageUrl
+    //     }
+    //   ]
+    // }
+  };
+}
+
+export default async function BlogPostPage({
+  params,
+}: BlogPostPageProps) {
+  const { postId } = await params
+  const response = await fetch(`https://dummyjson.com/posts/${postId}`);
+  const { title, body }: BlogPost = await response.json();
+
+  if (response.status === 404) {
+    notFound();
+  }
+
+  await delay(1000);
+
+  return (
+    <article className="max-w-prose m-auto space-y-5">
+      <h1 className="text-3xl text-center font-bold">{title}</h1>
+      <p className="text-lg">{body}</p>
+      <ClapButton />
+    </article>
+  );
+}
